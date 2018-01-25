d72bb232ea980af42ac3bf3b7195928146229e96
JasonChen <jason.chen@eventmsimple.com>
2018-01-25 18:10:14 +0800

    add Chen.Grpc

---
+++ src/Chen.Grpc/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chen.Grpc.Attributes
+{
+    /// <summary>
+    /// Don't register on Engine.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class IgnoreAttribute : Attribute
+    {
+    }
+}
